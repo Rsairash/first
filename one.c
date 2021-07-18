4ca5f462005dbf57c660db5ee0f5f8303775fdee
Sairash <sairash0323@gmail.com>
2021-07-18 15:32:14 +0545

    table program added

--- one.c
+++ one.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 int main()
 {
-    int a, b, sum;
+    int a, b, sum,i,j;
     a=12;
     b=34;
     sum= a+b;
     printf("The sum is %d",sum);
+
+    for(i=5;i<=10;i++)
+    {
+        for (j=1;j<=10;j++)
+        {
+            printf("%d * %d = %d ",i,j,i*j);
+            printf("\n");
+        }
+    }
 }
